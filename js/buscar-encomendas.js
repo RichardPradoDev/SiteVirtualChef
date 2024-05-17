9efa460e85164388c77a4ae974185e39b06af27f
Richard Prado <145171633+RichardPradoDev@users.noreply.github.com>
2024-05-16 22:14:16 -0300

    Add files via upload

---
+++ js/buscar-encomendas.js
@@ -0,0 +1,17 @@
+var botaoBuscar = document.querySelector("#buscar-encomendas");
+
+botaoBuscar.addEventListener("click", function () {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "http://localhost:3000/encomendas_web");
+    xhr.addEventListener("load", function () {
+        var resposta = xhr.responseText;
+        //Converte a String em objeto JS
+        var encomendas = JSON.parse(resposta);
+        //Insere as encomendas na tabela 
+        encomendas.forEach(function (encomenda) {
+            addEncomenda(encomenda)
+        })
+    });
+    xhr.send()
+})
